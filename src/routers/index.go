381fe26a2cdaa27f85255ac17dde91af71b94162
akmamun <almamunkhan04@gmail.com>
2022-03-11 10:34:26 +0600

    refactor route

--- src/routers/index.go
+++ src/routers/index.go
@@ -3,9 +3,16 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 //RegisterRoutes add all routing list here automatically get main router
 func RegisterRoutes(route *gin.Engine, db *gorm.DB) {
+	route.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Route Not Found"})
+	})
+	route.GET("/health", func(ctx *gin.Context) { ctx.JSON(http.StatusOK, gin.H{"like": "ok"}) })
+
+	//Add All route
 	TestRoutes(route, db)
 }
--- src/routers/router.go
+++ src/routers/router.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"gorm.io/gorm"
-	"net/http"
 	"pkg/src/routers/middleware"
 )
 
@@ -23,15 +22,6 @@ func Routes(db *gorm.DB) *gin.Engine {
 	router.Use(middleware.CORSMiddleware())
 
 	RegisterRoutes(router, db) //routes register
-
-	router.NoRoute(func(ctx *gin.Context) {
-		ctx.JSON(http.StatusNotFound,
-			gin.H{"status": http.StatusNotFound, "message": "Route Not Found"})
-	})
-
-	router.GET("/health", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "active": true})
-	})
 
 	return router
 }
