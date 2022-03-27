bcb8968c3b4fd8928b068f1edb75b5769f40caf2
mohammad.mamun <almamunkhan04@gmail.com>
2022-03-27 18:05:24 +0600

    added has many example

--- examples/ex_controllers/has_many_controller.go
+++ examples/ex_controllers/has_many_controller.go
@@ -4,12 +4,39 @@ import (
 	examples "gin-boilerplate/examples/ex_models"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
-// GetHasManyRelationData with Preload
-func (base *Controller) GetHasManyRelationData(ctx *gin.Context) {
+type CreditCardData struct {
+	Number string `json:"number"`
+}
+
+//GetHasManyRelationUserData fetch user data with preload
+func (base *Controller) GetHasManyRelationUserData(ctx *gin.Context) {
+	var user []examples.User
+	base.DB.Preload("CreditCards").Find(&user)
+	ctx.JSON(http.StatusOK, &user)
+
+}
+
+//GetHasManyRelationCreditCardData fetch credit-card data with preload
+func (base *Controller) GetHasManyRelationCreditCardData(ctx *gin.Context) {
 	var creditCards []examples.CreditCard
-	base.DB.Preload("CreditCards").Preload("User").Find(&creditCards)
+	base.DB.Find(&creditCards)
 	ctx.JSON(http.StatusOK, &creditCards)
 
+}
+
+// GetUserDetails based on user_id
+func (base *Controller) GetUserDetails(ctx *gin.Context) {
+	var user []examples.User
+	userId, _ := strconv.Atoi(ctx.DefaultQuery("user_id", ""))
+
+	err := base.DB.Preload("CreditCards").First(&user, userId).Error
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"user_id": "Enter valid user"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
 }
--- examples/ex_routes/example.go
+++ examples/ex_routes/example.go
@@ -9,6 +9,8 @@ import (
 func ExamplesRoutes(route *gin.Engine, db *gorm.DB) {
 	ctrl := ex_controllers.Controller{DB: db}
 	v1 := route.Group("/v1/examples")
-	v1.GET("test/", ctrl.GetHasManyRelationData)
+	v1.GET("test/", ctrl.GetHasManyRelationUserData)
+	v1.GET("test/card", ctrl.GetHasManyRelationCreditCardData)
+	v1.GET("test/user", ctrl.GetUserDetails)
 
 }
